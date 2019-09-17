037258b15f55d57c4da752a9d8d703bb46e406ae
Liam Wears <liamwears@fastmail.com>
2019-09-17 21:36:14 +0200

    cleaning up app.js

--- app.js
+++ app.js
@@ -23,62 +23,90 @@ function getPassword(length) {
 
 let rangeslider = document.getElementById("sliderRange"); 
 
-function changePassword(sliderValue) {
-    // let newPass1;
-    // let newPass2;
-    // let newPass3;
-    // let newPass4;
-    // let newPass5;
-    // let newPass6;
-    for (let i = 0; i < 3; i++) {
-        // need to change this!
-        let passwordToChange = passwordArray[i];
-        //let passToChange = document.getElementById( "demo" + i ).textContent;
+function changePassword (sliderValue) {
 
-        if (sliderValue == 0) {
-            document.getElementById( "demo" + i ).textContent = passwordArray[i];
+    // Should also be in external json
+    let letters = {
+        "a": "4",
+        "e": "3",
+        "o": "0",
+        "s": "5",
+        "l": "1",
+        "t": "7",
+        "z": "2"
+    }
+
+    for (let i = 0; i < 3; i++) {
+        let passwordToChange = passwordArray[i];
+
+        letterArray = Object.keys(letters);
+
+        for(let i = 0; i < sliderValue; i++){
+            let letter = letterArray[i];
+            let replacement = letters[letter];
+            passwordToChange = passwordToChange.replace(new RegExp(letter, "g"), replacement)
         }
-        if (sliderValue == 1) {
-            newPass1 = passwordToChange.replace(/a/g, '4');
-            document.getElementById( "demo" + i ).textContent = newPass1;
-        }
-        if (sliderValue == 2) {
-            newPass2 = passwordToChange.replace(/e/g, '3')
-                                        .replace(/a/g, '4');
-            document.getElementById( "demo" + i ).textContent = newPass2;
-        }
-        if (sliderValue == 3) {
-            newPass3 = passwordToChange.replace(/l/g, '1')
-                                        .replace(/e/g, '3')
-                                        .replace(/a/g, '4');
-            document.getElementById( "demo" + i ).textContent = newPass3;
-        }
-        if (sliderValue == 4) {
-            newPass4 = passwordToChange.replace(/o/g, '0')
-                                        .replace(/l/g, '1')
-                                        .replace(/e/g, '3')
-                                        .replace(/a/g, '4');
-            document.getElementById( "demo" + i ).textContent = newPass4;
-        }
-        if (sliderValue == 5) {
-            newPass5 = passwordToChange.replace(/s/g, '5')
-                                        .replace(/o/g, '0')
-                                        .replace(/l/g, '1')
-                                        .replace(/e/g, '3')
-                                        .replace(/a/g, '4');
-            document.getElementById( "demo" + i ).textContent = newPass5;
-        }
-        if (sliderValue == 6) {
-            newPass6 = passwordToChange.replace(/t/g, '7')
-                                        .replace(/s/g, '5')
-                                        .replace(/o/g, '0')
-                                        .replace(/l/g, '1')
-                                        .replace(/e/g, '3')
-                                        .replace(/a/g, '4');
-            document.getElementById( "demo" + i ).textContent = newPass6;
-        }
+        document.getElementById( "demo" + i ).textContent = passwordToChange
     }
 }
+
+
+// function changePassword(sliderValue) {
+//     // let newPass1;
+//     // let newPass2;
+//     // let newPass3;
+//     // let newPass4;
+//     // let newPass5;
+//     // let newPass6;
+//     for (let i = 0; i < 3; i++) {
+//         // need to change this!
+//         let passwordToChange = passwordArray[i];
+//         //let passToChange = document.getElementById( "demo" + i ).textContent;
+
+//         if (sliderValue == 0) {
+//             document.getElementById( "demo" + i ).textContent = passwordArray[i];
+//         }
+//         if (sliderValue == 1) {
+//             newPass1 = passwordToChange.replace(/a/g, '4');
+//             document.getElementById( "demo" + i ).textContent = newPass1;
+//         }
+//         if (sliderValue == 2) {
+//             newPass2 = passwordToChange.replace(/e/g, '3')
+//                                         .replace(/a/g, '4');
+//             document.getElementById( "demo" + i ).textContent = newPass2;
+//         }
+//         if (sliderValue == 3) {
+//             newPass3 = passwordToChange.replace(/l/g, '1')
+//                                         .replace(/e/g, '3')
+//                                         .replace(/a/g, '4');
+//             document.getElementById( "demo" + i ).textContent = newPass3;
+//         }
+//         if (sliderValue == 4) {
+//             newPass4 = passwordToChange.replace(/o/g, '0')
+//                                         .replace(/l/g, '1')
+//                                         .replace(/e/g, '3')
+//                                         .replace(/a/g, '4');
+//             document.getElementById( "demo" + i ).textContent = newPass4;
+//         }
+//         if (sliderValue == 5) {
+//             newPass5 = passwordToChange.replace(/s/g, '5')
+//                                         .replace(/o/g, '0')
+//                                         .replace(/l/g, '1')
+//                                         .replace(/e/g, '3')
+//                                         .replace(/a/g, '4');
+//             document.getElementById( "demo" + i ).textContent = newPass5;
+//         }
+//         if (sliderValue == 6) {
+//             newPass6 = passwordToChange.replace(/t/g, '7')
+//                                         .replace(/s/g, '5')
+//                                         .replace(/o/g, '0')
+//                                         .replace(/l/g, '1')
+//                                         .replace(/e/g, '3')
+//                                         .replace(/a/g, '4');
+//             document.getElementById( "demo" + i ).textContent = newPass6;
+//         }
+//     }
+// }
   
 rangeslider.oninput = function() { 
   rangeslider.innerHTML = this.value;
